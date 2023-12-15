61598c90cbf931c7ac61a5b9e1f8aab30dd6d47d
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2023-12-15 01:35:31 +0000

    Adding no-ansible-Manual_STIGS.txt for a list of STIGS that Ansible does not cover.

--- 15NOV2023/INSCOM-GISA-RHEL8.9-V1R12-STIG-MAC-II-Sensitive.bash
+++ 15NOV2023/INSCOM-GISA-RHEL8.9-V1R12-STIG-MAC-II-Sensitive.bash
@@ -24,6 +24,34 @@ function AIDEVAL {
 		esac
 	done
 }
+
+function SSHCRYPTO {
+	opt="$1"
+	str="$2"
+	cfg="/etc/crypto-policies/back-ends/opensshserver.config"
+	set="$(sed -e "s#-o#\n#g" $cfg | awk -F= -v opt="$opt" '$1 == opt {print $2}' | tr -d [:blank:])"
+	if [ "$set" != "$str" ]; then
+		sed -i "s#-o$opt=$set #-o$opt=$str #" $cfg
+	fi
+}
+
+function SSHDCFG {
+	setting="$1"
+	value="$2"
+	cfg="/etc/ssh/sshd_config"
+	cfgdir="$(dirname $cfg)"
+	cfgfile="$(basename $cfg)"
+	for file in $(find $cfgdir -type f -iname "$cfgfile*"); do
+		if [ "$(egrep -v -e "^#" $file | grep $setting | wc -l)" -lt "1" ]; then
+			printf "\n# %s - Adding \"%s %s\"\n%s %s\n\n" "$(date)" "$setting" "$value" "$setting" "$value" >> $file
+		else
+			if [ "$(awk -v setting="$setting" '$1 == setting {print $2}' $file)" != "$value" ]; then
+				sed -i "s#^$setting.*#\# $(date) - Modified $setting to $value\n$setting $value ---#g" $file
+			fi
+		fi
+	done
+}
+
 function V230475 {
 	for pkg in audit rsyslog; do
 		for bin in $(rpm -ql $pkg | grep bin); do
@@ -51,10 +79,25 @@ function V230263 {
 	fi
 }
 
+function V230252 {
+	SSHCRYPTO Ciphers aes256-ctr,aes192-ctr,aes128-ctr,aes256-gcm@openssh.com,aes128-gcm@openssh.com
+}
+
+function V255924 {
+	SSHCRYPTO KexAlgorithms ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512
+}
+
+function V244525 {
+	SSHDCFG ClientAliveInterval 600
+}
+
 function STIG {
-	V230475
-	V230551
-	V230552
-	V230263
+	#V230475
+	#V230551
+	#V230552
+	#V230263
+	#V230252
+	#V255924
+	V244525
 }
 STIG
